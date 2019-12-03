a59e6a0f697f32c2d2c9c5cdc4c6c56a8ad016e1
Michał Kubiak <michal.kubiak@famoc.com>
2019-12-03 15:29:27 +0100

    Step 2 - Implement basic input validation and XSS protection

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
-$name = $_GET['name'];
+$name = $_GET['name'] ?? 'World';
 
-echo 'hello ' . $name;
+header('Content-Type: text/html; charset=utf-8');
+
+printf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8'));

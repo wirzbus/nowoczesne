fa1f56d198ba68793acb3a9b459c812ec482e77b
Michał Kubiak <michal.kubiak@famoc.com>
2019-12-05 14:29:49 +0100

    Step 4 - Add bye endpoint

--- bye.php
+++ bye.php
@@ -6,7 +6,6 @@ use Symfony\Component\HttpFoundation\Response;
 require_once __DIR__ . '/vendor/autoload.php';
 
 $request = Request::createFromGlobals();
-$name = $request->get('name', 'World');
 
-$response = new Response(sprintf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
+$response = new Response('Bye!');
 $response->send();

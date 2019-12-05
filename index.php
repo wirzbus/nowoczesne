18ab17efc2497fc365f820d313eed786441b6359
Michał Kubiak <michal.kubiak@famoc.com>
2019-12-05 14:26:38 +0100

    Step 3 - Add HttpFoundation library

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
 
-$name = $_GET['name'] ?? 'World';
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-header('Content-Type: text/html; charset=utf-8');
+require_once __DIR__ . '/vendor/autoload.php';
 
-printf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8'));
+$request = Request::createFromGlobals();
+$name = $request->get('name', 'World');
 
-echo 'Czesc';
+$response = new Response(sprintf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
+$response->send();
+
+
